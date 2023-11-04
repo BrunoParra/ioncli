7c12a3d0a6ab12d1ce485a45b7996643361b2030
Bruno <brunoparraguez17@gmail.com>
2023-11-04 10:25:12 -0300

    objeto relacionado a implementacion Firebase

--- fastrip/src/environments/environment.ts
+++ fastrip/src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  firebaseConfig: {
+  apiKey: "AIzaSyDW5zV_qv_Bnfe7AJRJeFKrv5tDJEbkAUI",
+  authDomain: "testeofbion.firebaseapp.com",
+  projectId: "testeofbion",
+  storageBucket: "testeofbion.appspot.com",
+  messagingSenderId: "599242367157",
+  appId: "1:599242367157:web:a151245837dafd3ebd1efc",
+  measurementId: "G-ZHFQBQ81TY"
+}
 };
 
 /*
