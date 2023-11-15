db0e64d91a8ca13befde546addec0be64902b63f
Bruno <brunoparraguez17@gmail.com>
2023-11-15 03:30:48 -0300

    se añade servicio de interacciones con el usuario

--- fastrip/src/app/services/interaction.service.ts
+++ fastrip/src/app/services/interaction.service.ts
@@ -1,13 +1,18 @@
 import { Injectable } from '@angular/core';
-import { ToastController } from '@ionic/angular';
+import { LoadingController, ToastController } from '@ionic/angular';
 
 @Injectable({
   providedIn: 'root'
 })
 export class InteractionService {
+  //clase any para hacer uso de funciones de loadingController 
+  loading: any;
 
-  constructor(public toastController: ToastController) { }
+  constructor(public toastController: ToastController, 
+    public loadingController: LoadingController) { }
 
+
+  //con esto creamos un mensaje de alerta por 2 segundos que indica un mensaje a nuestra eleccion 
   async presentToast(mensaje: string) {
     const toast = await this.toastController.create({
       message: mensaje,
@@ -15,5 +20,23 @@ export class InteractionService {
     });
     toast.present();
   }
+
+
+  /*esto es para que se muestre un bloqueador de pantalla que nos 
+  muestra una rueda de carga junto con su funcion para que se valla*/
+  async presentLoading(mensaje: string){
+    this.loading = await this.loadingController.create({
+      cssClass: 'mi-mensaje-xd',
+      message:mensaje,
+    });
+    await this.loading.present()
+  }
+
+  async closeLoading() {
+    await this.loading.dismiss();
+  }
+
+
+
 
 }
