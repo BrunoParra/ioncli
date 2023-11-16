e7d4f086fd75eb8a6b3ad4e6ff5070783f06b34e
Bruno <brunoparraguez17@gmail.com>
2023-11-16 19:14:20 -0300

    se cambia carpeta login a inicio

---
+++ fastrip/src/app/pages/inicio/inicio.page.ts
@@ -0,0 +1,100 @@
+import { Component, OnInit } from '@angular/core';
+import { FirestoreService } from '../../services/firestore.service';
+import { User } from '../../models/models';
+import { Router } from '@angular/router';
+import { InteractionService } from '../../services/interaction.service';
+import { MenuController } from '@ionic/angular';
+import { RegistroserviceService } from '../../services/registroservice.service';
+
+@Component({
+  selector: 'app-inicio',
+  templateUrl: './inicio.page.html',
+  styleUrls: ['./inicio.page.scss'],
+})
+export class InicioPage{
+
+//ESTO HACE USO DEL FOR EACH QUE ESTA JUSTO ABAJO junto con el modelo que esta en models.ts
+//Esto es independiente de firebase. esto se almaceno en local.
+  usuarios: User [] = [
+    {
+      nombre: 'Amanda',
+      apepat: 'Gonzales',
+      apemat: 'Carrasco',
+      rut: '21554091k',
+      edad: 21,
+      genero: 'M',
+      correo: '',
+      contraseña: '',
+      chofer:{
+      patente: 'GISP45',
+      fabCoche: 'Nissan',
+      }
+    }
+  ]
+
+  usuario: any
+
+  //ESTO SIRVE PARA HACER USO DE SERVICIO DE DB
+  constructor(private firestore: FirestoreService, 
+            private router: Router,
+            private interaction: InteractionService,
+            private menuController: MenuController,
+            private serviceRegistro: RegistroserviceService) {
+    console.log('constructor se ejecuta antes que funcion getDriver ->') ;
+    //CON ESTE FOREACH TRAEMOS CADA USUARIO EN LA COLECCION DE "USUARIOS" QUE ESTA ARRIBA
+    this.usuarios.forEach(usuario => [
+      console.log('La edad es -> ', usuario.edad)
+    ] );
+    //ESTO ES PARA HACER USO DE LA FUNCION GetDriver DE MANERA AUTOMATICA Y QUE SE REFLEJE EN LA CONSOLA
+    this.getDriver();
+
+  }
+
+  async ngOnInit() {
+    this.usuario = await this.serviceRegistro.obtenerUsuario()
+  }
+
+  mostrarMenu(){
+    this.menuController.open('first');
+  }
+
+//AQUI ESTAN LOS DATOS JUNTO CON LA FUNCION PARA ENVIAR DATA POR EL MOMENTO NO AUTO RELLENADA A FIREBASE.
+//LA ESTRUCTURA SE ENCUENTRA EN models.ts
+  crearNuevoUsuario(){
+
+    this.interaction.presentLoading('Guardando. Porfavor Espere...');
+
+    const usuario: User = {
+    nombre: 'Florencia',
+    apepat: 'Correa',
+    apemat: 'Parraguez',
+    rut: '22.491.351-k',
+    edad: 19,
+    genero: 'M',
+    correo: 'flocor@gmail.com',
+    contraseña: 'contraseña',
+    }
+    /*CON ESTA MAMADA HACEMOS EL ROUTEO A FIREBASE DANDOLE UN COMBRE AL 'DOCUMENTO' SI NO EXISTE SE CREARA AUTOMATICO
+      LUEGO SE HACE LA RUTA QUE VA ASI. (data: any, path: string, id: string)
+      que te lo tradusco va asi:        (data = usuario es de tipo User que replica la estructura de la interface que esta en models.ts)
+                                        (path = a que documento de firebase se va a ir)
+                                        (id = vajo que nombre la coleccion se llamara)
+      */
+    console.log('Se envio info a firebase')
+    const path = 'Usuarios';
+    this.firestore.createUsr(usuario, path, 'testeo').then( () => {
+      console.log('Firebase devolvio token de respuesta.');
+      //con solo esto podemos hacer un pop up con el mensaje que queramos solo con agregar al constructor lo que requiere toast
+      this.interaction.closeLoading();
+      this.interaction.presentToast("Guardado con Exito");
+    });
+
+  }
+
+
+//HACE USO CONTROLADO DE LA FUNCION DE TRAER COLECCION DE FIREBASE
+  getDriver() {
+  this.firestore.getCollection();
+  }
+
+}
